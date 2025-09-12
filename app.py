803446c363bff73979b483678e015239993ee1dd
shriyashpachpande <shriyashpachpande813@gmail.com>
2025-09-12 22:42:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -119,4 +119,6 @@ def index():
     return render_template("index.html", result_html=result_html, error=error, groq_api_key=groq_api_key)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
